1e3fe8e59582fa9cc8fb48d747f02d40cfd68a7d
manjyot sandhu <manjyotsandhu@gmail.com>
2017-02-20 14:02:35 +0000

    Added from cloud 9 test

--- src/javatest/loltest.java
+++ src/javatest/loltest.java
@@ -3,7 +3,7 @@ package javatest;
 public class loltest {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    System.out.println("editted from cloud9");
 
   }
 
